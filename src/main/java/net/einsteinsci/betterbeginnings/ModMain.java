4fdb322d64597a2730f06a1122c174ce6d743be8
PitchBright <johnnytweed@hotmail.com>
2016-01-27 18:04:47 -0500

    Versioning

--- src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
+++ src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
@@ -32,7 +32,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ModMain
 {
 	public static final String MODID = "betterbeginnings";
-	public static final String VERSION = "0.9.5-R6";
+	public static final String VERSION = "0.9.5-Khufu";
 	public static final String NAME = "Better Beginnings";
 	public static final CreativeTabs tabBetterBeginnings = new CreativeTabs("tabBetterBeginnings")
 	{
